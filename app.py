0c27654aeb4b666e4b6b4d412b957bb99cba38ad
bmason30 <45826014+bmason30@users.noreply.github.com>
2025-07-21 17:59:15 -0400

    Update app.py
    v3

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
 import warnings
-from scipy import stats
 
 # Suppress only specific warnings, not all
 warnings.filterwarnings('ignore', category=FutureWarning, module='yfinance')

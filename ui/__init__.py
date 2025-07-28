5e034b872484b3b3ff7de4d45d06c582caab20cc
bmason30 <45826014+bmason30@users.noreply.github.com>
2025-07-28 15:11:59 -0400

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,13 @@
+"""
+UI components module for VWV Trading System
+"""
 
+from .components import (
+    create_technical_score_bar,
+    create_header
+)
+
+__all__ = [
+    'create_technical_score_bar',
+    'create_header'
+]
